3c075cdd1c9a4b66827d7bde58894bdaa892f4d5
EC2 Default User <ec2-user@ip-172-31-21-150.ap-northeast-1.compute.internal>
2020-12-09 08:15:23 +0000

    コントローラー編集

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,12 +1,42 @@
 class CardsController < ApplicationController
+  layout 'cards'
   def index
+    @cards = Card.all
   end
 
   def show
+    @cards = Card.find(params[:id])
   end
+  
   def add
+    if requset.post? then
+      Card.create(card_params)
+      goback
+    else
+      @card = Card.new
+    end
   end
 
-  def show
+  def edit
+    @card = Card.find(params[:id])
+    if request.patch? then
+      @card.update(card_params)
+      goback
+    end
+  end
+  
+  def delete
+    Card.find(params[:id])
+    Card.destroy
+    goback
+  end
+  
+  private
+  def card_params
+    params.require(:card).permit(:title, :suthor, :price, :pubulisher, :memo)
+  end
+  
+  def goback
+    redirect_to '/cards'
   end
 end
